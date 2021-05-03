c7ca224840ad875058a359f9c95441a42f1d822c
github username <github email address>
2021-05-03 01:11:40 -0500

    done

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -5,8 +5,7 @@ class ToyForm extends Component {
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
-          <h3>Create a toy!</h3>
+        <form className="add-toy-form" onSubmit={this.props.createNewToy}>          <h3>Create a toy!</h3>
           <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
           <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
